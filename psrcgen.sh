384e0a2696b458be172ed9ed1331dccfc600aa96
ohilho <iroiro515@gmail.com>
2020-05-30 02:18:02 +0900

    create cmakelists.txt file when necessary

--- psrcgen.sh
+++ psrcgen.sh
@@ -27,18 +27,67 @@ test_dir=${git_root}/test/${package_name_original}/${module_name_original}
 # generate year
 YEAR=$(date +%Y)
 
-# Create directories if the directories does not exists.
+# Create top level CMakeLists.txt file if the files does not exists.
+if [ ! -f "${git_root}/CMakeLists.txt" ]; then
+  sed \
+  -e "s/\${module_name_capital}/${module_name_capital}/" \
+  -e "s/\${module_name_original}/${module_name_original}/" \
+  -e "s/\${file_name_original}/${file_name_original}/" \
+  -e "s/\${file_name_capital}/${file_name_capital}/" \
+  -e "s/\${package_name_original}/${package_name_original}/" \
+  -e "s/\${package_name_capital}/${package_name_capital}/" \
+  -e "s/\${year}/${YEAR}/" \
+  ${BASEDIR}/CMakeLists.txt.template > ${git_root}/CMakeLists.txt
+fi
+
+
+# Initialize include dir
+if [ ! -d "${git_root}/include" ]; then
+  mkdir -p ${git_root}/include
+fi
+
+# Initialize src dir
+if [ ! -d "${git_root}/src" ]; then
+  mkdir -p ${git_root}/src
+fi
+
+if [ ! -f "${git_root}/src/CMakeLists.txt" ]; then
+  touch "${git_root}/src/CMakeLists.txt"
+fi
+
+# Initialize test dir
+if [ ! -d "${git_root}/test" ]; then
+  mkdir -p ${git_root}/test
+fi
+
+if [ ! -f "${git_root}/test/CMakeLists.txt" ]; then
+  touch "${git_root}/test/CMakeLists.txt"
+fi
+
+
+# Initialize header dir
 if [ ! -d "${header_dir}" ]; then
   mkdir -p ${header_dir}
 fi
 
+# Initialize source dir
 if [ ! -d "${source_dir}" ]; then
   mkdir -p ${source_dir}
 fi
+if [ ! -f "${source_dir}/CMakeLists.txt" ]; then
+  touch "${source_dir}/CMakeLists.txt"
+  echo "add_subdirectory(${package_name_original}/${module_name_original})" >> "${git_root}/src/CMakeLists.txt"
+fi
 
+# Initialize test dir
 if [ ! -d "${test_dir}" ]; then
   mkdir -p ${test_dir}
 fi
+if [ ! -f "${test_dir}/CMakeLists.txt" ]; then
+  touch "${test_dir}/CMakeLists.txt"
+  echo "add_subdirectory(${package_name_original}/${module_name_original})" >> "${git_root}/test/CMakeLists.txt"
+fi
+
 
 # Create *.cpp, *.h, *_test.cpp files if the files does not exists.
 if [ ! -f "${header_dir}/${file_name_original}.h" ]; then
@@ -77,14 +126,16 @@ if [ ! -f "${test_dir}/${file_name_original}_test.cpp" ]; then
   ${BASEDIR}/gtest.template > ${test_dir}/${file_name_original}_test.cpp
 fi
 
-if [ ! -f "${test_dir}/${module_name_original}_gtest_main_test.cpp" ]; then
-  sed \
-  -e "s/\${module_name_capital}/${module_name_capital}/" \
-  -e "s/\${module_name_original}/${module_name_original}/" \
-  -e "s/\${file_name_original}/${file_name_original}/" \
-  -e "s/\${file_name_capital}/${file_name_capital}/" \
-  -e "s/\${package_name_original}/${package_name_original}/" \
-  -e "s/\${package_name_capital}/${package_name_capital}/" \
-  -e "s/\${year}/${YEAR}/" \
-  ${BASEDIR}/gtest_main.template > ${test_dir}/${module_name_original}_test.cpp
-fi
+
+# deprecated. use libgtest_main.a
+# if [ ! -f "${test_dir}/${module_name_original}_gtest_main_test.cpp" ]; then
+#   sed \
+#   -e "s/\${module_name_capital}/${module_name_capital}/" \
+#   -e "s/\${module_name_original}/${module_name_original}/" \
+#   -e "s/\${file_name_original}/${file_name_original}/" \
+#   -e "s/\${file_name_capital}/${file_name_capital}/" \
+#   -e "s/\${package_name_original}/${package_name_original}/" \
+#   -e "s/\${package_name_capital}/${package_name_capital}/" \
+#   -e "s/\${year}/${YEAR}/" \
+#   ${BASEDIR}/gtest_main.template > ${test_dir}/${module_name_original}_test.cpp
+# fi
